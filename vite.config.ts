999fb037be7efca2ec2c4c5860c530e62b1abb0b
Tom Carter <tomcarter107@gmail.com>
2024-09-20 18:20:13 +0100

    chore: Setup RTL tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import eslint from "@nabla/vite-plugin-eslint";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
@@ -6,4 +7,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [eslint(), react(), tsconfigPaths()],
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
+  },
 });
