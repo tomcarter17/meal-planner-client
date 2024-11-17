df5a7289a0e4b8b3d4c1f878f8bab4d617264b84
Tom Carter <tomcarter107@gmail.com>
2024-11-17 15:44:33 +0000

    chore: Remove Chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import { type StorybookConfig } from "@storybook/react-vite";
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@chromatic-com/storybook",
     "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
